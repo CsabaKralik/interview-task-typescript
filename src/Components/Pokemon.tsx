bf323ef911c4a93560d0218772615a56223f6479
CsabaKralik <kralikcsabi@gmail.com>
2022-07-24 20:39:18 +0200

    add pokemon component with minimal style

--- src/Components/Pokemon.tsx
+++ src/Components/Pokemon.tsx
@@ -1,13 +1,30 @@
-interface Pokemon {
-  name: string;
-  weight: number;
-}
+import './pokemon.css';
 
-export default function Pokemoncard({ name, weight }:Pokemon) {
+import { useContext, useEffect, useState } from 'react';
+
+import UserContext from './UserContext';
+
+export default function Pokemoncard() {
+  const [pokemon, setpokemon] = useState<any>();
+  const [context, SetContext] = useContext<any>(UserContext);
+  const fetchPokemon = async () => {
+    const rndmNumber = Math.floor(Math.random() * 1153) + 1;
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${rndmNumber}`);
+    const json = await response.json();
+    setpokemon(json);
+  };
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  const buy = () => {
+    SetContext(context.append({ name: pokemon.name, weight: pokemon.weight }));
+  };
   return (
-    <div>
-      <div>{name}</div>
-      <div>{weight}</div>
+    <div className="pokemon-card">
+      <div className="pokemon-name">{pokemon ? pokemon.name : 'loading'}</div>
+      <div className="pokemon-price">{pokemon ? `${pokemon.weight * 100}$` : 'loading'}</div>
+      <button type="button" onClick={buy}>Buy</button>
     </div>
   );
 }
