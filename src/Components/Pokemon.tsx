4755eb135531b5b51630c3bfa732d651d7c3d30c
CsabaKralik <kralikcsabi@gmail.com>
2022-07-25 23:35:52 +0200

    update pokemon component

--- src/Components/Pokemon.tsx
+++ src/Components/Pokemon.tsx
@@ -2,13 +2,14 @@ import './pokemon.css';
 
 import { useContext, useEffect, useState } from 'react';
 
+import pokemonInterface from './pokemon.1';
 import UserContext from './UserContext';
 
-export default function Pokemoncard() {
+export default function Pokemoncard({ name, weight } :pokemonInterface) {
   const [pokemon, setpokemon] = useState<any>();
-  const [context, SetContext] = useContext<any>(UserContext);
+  const { value, SetValue } = useContext<any>(UserContext);
   const fetchPokemon = async () => {
-    const rndmNumber = Math.floor(Math.random() * 1153) + 1;
+    const rndmNumber = Math.floor(Math.random() * 905) + 1;
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${rndmNumber}`);
     const json = await response.json();
     setpokemon(json);
@@ -18,13 +19,27 @@ export default function Pokemoncard() {
   }, []);
 
   const buy = () => {
-    SetContext(context.append({ name: pokemon.name, weight: pokemon.weight }));
+    SetValue([...value, { id: pokemon.id, name: pokemon.name, weight: pokemon.weight }]);
+    document.getElementById(pokemon.name)?.remove();
   };
   return (
     <div className="pokemon-card">
-      <div className="pokemon-name">{pokemon ? pokemon.name : 'loading'}</div>
-      <div className="pokemon-price">{pokemon ? `${pokemon.weight * 100}$` : 'loading'}</div>
-      <button type="button" onClick={buy}>Buy</button>
+      {/* needs id */}
+      {name && weight
+        ? (
+          <div>
+            <div className="pokemon-name">{name}</div>
+            <div className="pokemon-price">{weight}</div>
+            <div className="pokemon-price">{`${weight * 100}$`}</div>
+          </div>
+        )
+        : (
+          <div>
+            <div className="pokemon-name">{pokemon ? pokemon.name : 'loading'}</div>
+            <div className="pokemon-price">{pokemon ? `${pokemon.weight * 100}$` : 'loading'}</div>
+            <button type="button" onClick={buy}>Buy</button>
+          </div>
+        )}
     </div>
   );
 }
