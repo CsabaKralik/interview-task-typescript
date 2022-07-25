576453617c3ad2f1561b7fe7d216d2cc3ff1ed8e
CsabaKralik <kralikcsabi@gmail.com>
2022-07-25 23:35:22 +0200

    add context

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
+
+import UserContext from './Components/UserContext';
 import MainPage from './pages/MainPage';
 
 function App() {
-  return <MainPage />;
+  const [value, SetValue] = useState<any>([]);
+  return (
+    <UserContext.Provider value={{ value, SetValue }}>
+      <MainPage />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
